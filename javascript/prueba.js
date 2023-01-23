16f9f3781c99676c710f58e5ebc1bcd3491a0952
LuisAntonioMendozaRamirez <luis_pelos_ops@hotmail.com>
2023-01-23 11:56:48 -0600

    proyecto en github

---
+++ javascript/prueba.js
@@ -0,0 +1,465 @@
+function enviar()
+{
+    let R1 = document.getElementById("p1").checked;
+    let R2 = document.getElementById("p2").checked;
+    let R3 = document.getElementById("p3").checked;
+    let R4 = document.getElementById("p4").checked;
+    let R5 = document.getElementById("p5").checked;
+    let init_R1 = 0
+    if (R1) {
+        init_R1 = 1
+    }
+    if (R2) {
+        init_R1 = 2
+    }
+    if (R3) {
+        init_R1 = 3
+    }
+    if (R4) {
+        init_R1 = 4
+    }
+    if (R5) {
+        init_R1 = 5
+    }
+    console.log("Respuesta a pregunta 1: " + init_R1)
+    
+    let R6 = document.getElementById("p6").checked;
+    let R7 = document.getElementById("p7").checked;
+    let R8 = document.getElementById("p8").checked;
+    let R9 = document.getElementById("p9").checked;
+    let R10 = document.getElementById("p10").checked;
+    let init_R2 = 0
+    if (R6) {
+        init_R2 = 1
+    }
+    if (R7) {
+        init_R2 = 2
+    }
+    if (R8) {
+        init_R2 = 3
+    }
+    if (R9) {
+        init_R2 = 4
+    }
+    if (R10) {
+        init_R2 = 5
+    }
+    console.log("Respuesta a pregunta 2: " + init_R2)
+
+    let R11 = document.getElementById("p11").checked;
+    let R12 = document.getElementById("p12").checked;
+    let R13 = document.getElementById("p13").checked;
+    let R14 = document.getElementById("p14").checked;
+    let R15 = document.getElementById("p15").checked;
+    let init_R3 = 0
+    if (R11) {
+        init_R3 = 1
+    }
+    if (R12) {
+        init_R3 = 2
+    }
+    if (R13) {
+        init_R3 = 3
+    }
+    if (R14) {
+        init_R3 = 4
+    }
+    if (R15) {
+        init_R3 = 5
+    }
+    console.log("Respuesta a pregunta 3: " + init_R3)
+
+    let R16 = document.getElementById("p16").checked;
+    let R17 = document.getElementById("p17").checked;
+    let R18 = document.getElementById("p18").checked;
+    let R19 = document.getElementById("p19").checked;
+    let R20 = document.getElementById("p20").checked;
+    let init_R4 = 0
+    if (R16) {
+        init_R4 = 1
+    }
+    if (R17) {
+        init_R4 = 2
+    }
+    if (R18) {
+        init_R4 = 3
+    }
+    if (R19) {
+        init_R4 = 4
+    }
+    if (R20) {
+        init_R4 = 5
+    }
+    console.log("Respuesta a pregunta 4: " + init_R4)
+
+    let R21 = document.getElementById("p21").checked;
+    let R22 = document.getElementById("p22").checked;
+    let R23 = document.getElementById("p23").checked;
+    let R24 = document.getElementById("p24").checked;
+    let R25 = document.getElementById("p25").checked;
+    let init_R5 = 0
+    if (R21) {
+        init_R5 = 1
+    }
+    if (R22) {
+        init_R5 = 2
+    }
+    if (R23) {
+        init_R5 = 3
+    }
+    if (R24) {
+        init_R5 = 4
+    }
+    if (R25) {
+        init_R5 = 5
+    }
+    console.log("Respuesta a pregunta 5: " + init_R5)
+
+    let R26 = document.getElementById("p26").checked;
+    let R27 = document.getElementById("p27").checked;
+    let R28 = document.getElementById("p28").checked;
+    let R29 = document.getElementById("p29").checked;
+    let R30 = document.getElementById("p30").checked;
+    let init_R6 = 0
+    if (R26) {
+        init_R6 = 1
+    }
+    if (R27) {
+        init_R6 = 2
+    }
+    if (R28) {
+        init_R6 = 3
+    }
+    if (R29) {
+        init_R6 = 4
+    }
+    if (R30) {
+        init_R6 = 5
+    }
+    console.log("Respuesta a pregunta 6: " + init_R6)
+
+    let R31 = document.getElementById("p31").checked;
+    let R32 = document.getElementById("p32").checked;
+    let R33 = document.getElementById("p33").checked;
+    let R34 = document.getElementById("p34").checked;
+    let R35 = document.getElementById("p35").checked;
+    let init_R7 = 0
+    if (R31) {
+        init_R7 = 1
+    }
+    if (R32) {
+        init_R7 = 2
+    }
+    if (R33) {
+        init_R7 = 3
+    }
+    if (R34) {
+        init_R7 = 4
+    }
+    if (R35) {
+        init_R7 = 5
+    }
+    console.log("Respuesta a pregunta 7: " + init_R7)
+
+    let R36 = document.getElementById("p36").checked;
+    let R37 = document.getElementById("p37").checked;
+    let R38 = document.getElementById("p38").checked;
+    let R39 = document.getElementById("p39").checked;
+    let R40 = document.getElementById("p40").checked;
+    let init_R8 = 0
+    if (R36) {
+        init_R8 = 1
+    }
+    if (R37) {
+        init_R8 = 2
+    }
+    if (R38) {
+        init_R8 = 3
+    }
+    if (R39) {
+        init_R8 = 4
+    }
+    if (R40) {
+        init_R8 = 5
+    }
+    console.log("Respuesta a pregunta 8: " + init_R8)
+
+    let R41 = document.getElementById("p41").checked;
+    let R42 = document.getElementById("p42").checked;
+    let R43 = document.getElementById("p43").checked;
+    let R44 = document.getElementById("p44").checked;
+    let R45 = document.getElementById("p45").checked;
+    let init_R9 = 0
+    if (R41) {
+        init_R9 = 1
+    }
+    if (R42) {
+        init_R9 = 2
+    }
+    if (R43) {
+        init_R9 = 3
+    }
+    if (R44) {
+        init_R9 = 4
+    }
+    if (R45) {
+        init_R9 = 5
+    }
+    console.log("Respuesta a pregunta 9: " + init_R9)
+
+    let R46 = document.getElementById("p46").checked;
+    let R47 = document.getElementById("p47").checked;
+    let R48 = document.getElementById("p48").checked;
+    let R49 = document.getElementById("p49").checked;
+    let R50 = document.getElementById("p50").checked;
+    let init_R10 = 0
+    if (R46) {
+        init_R10 = 1
+    }
+    if (R47) {
+        init_R10 = 2
+    }
+    if (R48) {
+        init_R10 = 3
+    }
+    if (R49) {
+        init_R10 = 4
+    }
+    if (R50) {
+        init_R10 = 5
+    }
+    console.log("Respuesta a pregunta 10: " + init_R10)
+
+    let R51 = document.getElementById("p51").checked;
+    let R52 = document.getElementById("p52").checked;
+    let R53 = document.getElementById("p53").checked;
+    let R54 = document.getElementById("p54").checked;
+    let R55 = document.getElementById("p55").checked;
+    let init_R11 = 0
+    if (R51) {
+        init_R11 = 1
+    }
+    if (R52) {
+        init_R11 = 2
+    }
+    if (R53) {
+        init_R11 = 3
+    }
+    if (R54) {
+        init_R11 = 4
+    }
+    if (R55) {
+        init_R11 = 5
+    }
+    console.log("Respuesta a pregunta 11: " + init_R11)
+
+    let R56 = document.getElementById("p56").checked;
+    let R57 = document.getElementById("p57").checked;
+    let R58 = document.getElementById("p58").checked;
+    let R59 = document.getElementById("p59").checked;
+    let R60 = document.getElementById("p60").checked;
+    let init_R12 = 0
+    if (R56) {
+        init_R12 = 1
+    }
+    if (R57) {
+        init_R12 = 2
+    }
+    if (R58) {
+        init_R12 = 3
+    }
+    if (R59) {
+        init_R12 = 4
+    }
+    if (R60) {
+        init_R12 = 5
+    }
+    console.log("Respuesta a pregunta 12: " + init_R12)
+
+    let R61 = document.getElementById("p61").checked;
+    let R62 = document.getElementById("p62").checked;
+    let R63 = document.getElementById("p63").checked;
+    let R64 = document.getElementById("p64").checked;
+    let R65 = document.getElementById("p65").checked;
+    let init_R13 = 0
+    if (R61) {
+        init_R13 = 1
+    }
+    if (R62) {
+        init_R13 = 2
+    }
+    if (R63) {
+        init_R13 = 3
+    }
+    if (R64) {
+        init_R13 = 4
+    }
+    if (R65) {
+        init_R13 = 5
+    }
+    console.log("Respuesta a pregunta 13: " + init_R13)
+
+    let R66 = document.getElementById("p66").checked;
+    let R67 = document.getElementById("p67").checked;
+    let R68 = document.getElementById("p68").checked;
+    let R69 = document.getElementById("p69").checked;
+    let R70 = document.getElementById("p70").checked;
+    let init_R14 = 0
+    if (R66) {
+        init_R14 = 1
+    }
+    if (R67) {
+        init_R14 = 2
+    }
+    if (R68) {
+        init_R14 = 3
+    }
+    if (R69) {
+        init_R14 = 4
+    }
+    if (R70) {
+        init_R14 = 5
+    }
+    console.log("Respuesta a pregunta 14: " + init_R14)
+
+    let R71 = document.getElementById("p71").checked;
+    let R72 = document.getElementById("p72").checked;
+    let R73 = document.getElementById("p73").checked;
+    let R74 = document.getElementById("p74").checked;
+    let R75 = document.getElementById("p75").checked;
+    let init_R15 = 0
+    if (R71) {
+        init_R15 = 1
+    }
+    if (R72) {
+        init_R15 = 2
+    }
+    if (R73) {
+        init_R15 = 3
+    }
+    if (R74) {
+        init_R15 = 4
+    }
+    if (R75) {
+        init_R15 = 5
+    }
+    console.log("Respuesta a pregunta 15: " + init_R15)
+
+    let R76 = document.getElementById("p76").checked;
+    let R77 = document.getElementById("p77").checked;
+    let R78 = document.getElementById("p78").checked;
+    let R79 = document.getElementById("p79").checked;
+    let R80 = document.getElementById("p80").checked;
+    let init_R16 = 0
+    if (R76) {
+        init_R16 = 1
+    }
+    if (R77) {
+        init_R16 = 2
+    }
+    if (R78) {
+        init_R16 = 3
+    }
+    if (R79) {
+        init_R16 = 4
+    }
+    if (R80) {
+        init_R16 = 5
+    }
+    console.log("Respuesta a pregunta 16: " + init_R16)
+
+    let R81 = document.getElementById("p81").checked;
+    let R82 = document.getElementById("p82").checked;
+    let R83 = document.getElementById("p83").checked;
+    let R84 = document.getElementById("p84").checked;
+    let R85 = document.getElementById("p84").checked;
+    let init_R17 = 0
+    if (R81) {
+        init_R17 = 1
+    }
+    if (R82) {
+        init_R17 = 2
+    }
+    if (R83) {
+        init_R17 = 3
+    }
+    if (R84) {
+        init_R17 = 4
+    }
+    if (R85) {
+        init_R17 = 5
+    }
+    console.log("Respuesta a pregunta 17: " + init_R17)
+
+    let R86 = document.getElementById("p86").checked;
+    let R87 = document.getElementById("p87").checked;
+    let R88 = document.getElementById("p88").checked;
+    let R89 = document.getElementById("p89").checked;
+    let R90 = document.getElementById("p90").checked;
+    let init_R18 = 0
+    if (R86) {
+        init_R18 = 1
+    }
+    if (R87) {
+        init_R18 = 2
+    }
+    if (R88) {
+        init_R18 = 3
+    }
+    if (R89) {
+        init_R18 = 4
+    }
+    if (R90) {
+        init_R18 = 5
+    }
+    console.log("Respuesta a pregunta 18: " + init_R18)
+
+    let R91 = document.getElementById("p91").checked;
+    let R92 = document.getElementById("p92").checked;
+    let R93 = document.getElementById("p93").checked;
+    let R94 = document.getElementById("p94").checked;
+    let R95 = document.getElementById("p95").checked;
+    let init_R19 = 0
+    if (R91) {
+        init_R19 = 1
+    }
+    if (R92) {
+        init_R19 = 2
+    }
+    if (R93) {
+        init_R19 = 3
+    }
+    if (R94) {
+        init_R19 = 4
+    }
+    if (R95) {
+        init_R19 = 5
+    }
+    console.log("Respuesta a pregunta 19: " + init_R19)
+
+    let R96 = document.getElementById("p96").checked;
+    let R97 = document.getElementById("p97").checked;
+    let R98 = document.getElementById("p98").checked;
+    let R99 = document.getElementById("p99").checked;
+    let R100 = document.getElementById("p100").checked;
+    let init_R20 = 0
+    if (R96) {
+        init_R20 = 1
+    }
+    if (R97) {
+        init_R20 = 2
+    }
+    if (R98) {
+        init_R20 = 3
+    }
+    if (R99) {
+        init_R20 = 4
+    }
+    if (R100) {
+        init_R20 = 5
+    }
+    console.log("Respuesta a pregunta 20: " + init_R20)
+
+    let comentarios = document.getElementById("comentarios").value;
+    console.log("los comentarios son : " + comentarios)
+}
